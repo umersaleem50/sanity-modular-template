c7d7ac4e069a337df6ef812e2ed3767925685dd6
SamarRian <samarrian$@gmail.com>
2026-01-27 20:22:44 +0500

    +Navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Navbar from "@/components/Navbar";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
@@ -5,6 +6,7 @@ export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+        <Navbar />
         <Button variant={"destructive"}>Click me</Button>
       </main>
     </div>

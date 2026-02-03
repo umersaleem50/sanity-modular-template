6efc8f18bcd2698027ea3ec7a5aafabc85e518b6
Samar Rian <samarrian4@email.com>
2026-02-03 17:46:07 +0500

    leni-updated

--- components/ui/SmoothScroll.tsx
+++ components/ui/SmoothScroll.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { ReactNode, useEffect } from "react";
-import Lenis from "@studio-freight/lenis";
+import Lenis from "lenis";
 
 export default function SmoothScroll({ children }: { children: ReactNode }) {
   useEffect(() => {
